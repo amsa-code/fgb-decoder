54205a2f45116e1253e56a86e7999622b6b8767d
Dave Moten <davidmoten@gmail.com>
2021-03-10 09:36:28 +1100

    skip 1000 files in coverage.sh

--- coverage.sh
+++ coverage.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 set -e
 count=`(mvn jacoco:prepare-agent test jacoco:report >/dev/null && cat  target/site/jacoco/jacoco.xml|sed -e 's/\/>/\/>\n/g'|grep counter|tail -5|sed -e 's/.*missed="\([0-9]*\)".*/\1/g'|awk '{sum+=$1}END{print sum}')`
+counter=0
 for f in $HOME/files/*
 do 
+  ((counter=counter+1))
+  if (( counter < 1000)); then
   echo ------------------------------------------
   filename=${f:16}
   dest="src/test/resources/compliance-kit/$filename"
@@ -16,6 +19,7 @@ do
     echo removing $filename because missed coverage did not reduce 
     count=$previous
     rm $dest 
+  fi
   fi
   fi
 done
