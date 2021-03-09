86a87e537c242d8e591a78b0e5339e1db83e7c71
David Moten <davidmoten@gmail.com>
2021-03-09 14:20:10 +1100

    coverage.sh dev

--- coverage.sh
+++ coverage.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
-(mvn jacoco:prepare-agent test jacoco:report >/dev/null 2>&1) && cat  target/site/jacoco/jacoco.xml|sed -e 's/\/>/\/>\n/g'|grep counter|tail -5|sed -e 's/.*missed="\([0-9]*\)".*/\1/g'|awk '{sum+=$1}END{print sum}'
-
+set -e
+set -x
+count=0
+for f in target/files/*
+do 
+  echo $f 
+  echo -------------------
+  filename=${f:13}
+  cp $f "src/test/resources/compliance-kit/$filename" 
+  previous=$count
+  count=`(mvn jacoco:prepare-agent test jacoco:report >/dev/null && cat  target/site/jacoco/jacoco.xml|sed -e 's/\/>/\/>\n/g'|grep counter|tail -5|sed -e 's/.*missed="\([0-9]*\)".*/\1/g'|awk '{sum+=$1}END{print sum}')`
+  echo count = $count
+  if (( previous >= count )); then
+    echo removing $filename because coverage did not increase
+    count=$previous
+    rm "src/test/resources/compliance-kit/$filename" 
+  fi
+done
