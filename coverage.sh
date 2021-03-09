f3b3686ac7da18bef0199823f9366e0b536553c2
David Moten <davidmoten@gmail.com>
2021-03-09 15:12:06 +1100

    coverage.sh dev

--- coverage.sh
+++ coverage.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-count=0
+count=1000000
 for f in /home/dxm/files/*
 do 
   echo $f 
@@ -12,8 +12,8 @@ do
   previous=$count
   count=`(mvn jacoco:prepare-agent test jacoco:report >/dev/null && cat  target/site/jacoco/jacoco.xml|sed -e 's/\/>/\/>\n/g'|grep counter|tail -5|sed -e 's/.*missed="\([0-9]*\)".*/\1/g'|awk '{sum+=$1}END{print sum}')`
   echo count = $count
-  if (( previous >= count )); then
-    echo removing $filename because coverage did not increase
+  if (( previous <= count )); then
+    echo removing $filename because missed coverage did not reduce 
     count=$previous
     rm $dest 
   fi
