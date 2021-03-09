7a78096795c204221e456289744086e64f056c00
David Moten <davidmoten@gmail.com>
2021-03-09 15:13:59 +1100

    coverage.sh dev

--- coverage.sh
+++ coverage.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-count=1000000
+count=`(mvn jacoco:prepare-agent test jacoco:report >/dev/null && cat  target/site/jacoco/jacoco.xml|sed -e 's/\/>/\/>\n/g'|grep counter|tail -5|sed -e 's/.*missed="\([0-9]*\)".*/\1/g'|awk '{sum+=$1}END{print sum}')`
 for f in /home/dxm/files/*
 do 
   echo ------------------------------------------
