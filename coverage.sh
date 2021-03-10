4b8d5a4c291974a7517ed9100ecb692953d20309
David Moten <davidmoten@gmail.com>
2021-03-10 11:39:43 +1100

    reduce size of compliance-kit by checking contribution to test coverage

--- coverage.sh
+++ coverage.sh
@@ -2,10 +2,10 @@
 set -e
 count=`(mvn jacoco:prepare-agent test jacoco:report >/dev/null && cat  target/site/jacoco/jacoco.xml|sed -e 's/\/>/\/>\n/g'|grep counter|tail -5|sed -e 's/.*missed="\([0-9]*\)".*/\1/g'|awk '{sum+=$1}END{print sum}')`
 counter=0
-for f in $HOME/files/*
+for f in $HOME/files/*;
 do 
   ((counter=counter+1))
-  if (( counter > 1000)); then
+  if (( counter > 0)); then
   echo ------------------------------------------
   filename=${f:16}
   dest="src/test/resources/compliance-kit/$filename"
