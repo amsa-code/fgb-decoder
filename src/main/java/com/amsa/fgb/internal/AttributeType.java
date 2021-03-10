00130a0cdbabdeb526ccee2a1d761ee249d7427b
David Moten <davidmoten@gmail.com>
2021-03-10 14:42:42 +1100

    add some json types for AttributeType

--- src/main/java/com/amsa/fgb/internal/AttributeType.java
+++ src/main/java/com/amsa/fgb/internal/AttributeType.java
@@ -1,44 +1,45 @@
 package com.amsa.fgb.internal;
 
 public enum AttributeType {
-    _121_5_MHZ_HOMING("121.5 MHz Homing"), //
+    _121_5_MHZ_HOMING("121.5 MHz Homing", JsonType.BOOLEAN), //
     ACTIVATION_TYPE("Activation Type"), //
     ADDITIONAL_DATA_FLAG("Additional Data Flag"), //
-    ADDITIONAL_ELT_NUMBERS("Additional ELT numbers"), AIRCRAFT_24_BIT_ADDRESS_BINARY("Aircraft 24 bit address Binary"), //
+    ADDITIONAL_ELT_NUMBERS("Additional ELT numbers", JsonType.INTEGER), //
+    AIRCRAFT_24_BIT_ADDRESS_BINARY("Aircraft 24 bit address Binary"), //
     AIRCRAFT_24_BIT_ADDRESS_HEX("Aircraft 24 bit address (hex)"), //
     AIRCRAFT_24_BIT_ADDRESS_OCTAL("Aircraft 24 bit address (octal)"), //
     AIRCRAFT_CALL_SIGN("Aircraft Reg. Marking"), //
     AIRCRAFT_OPERATOR("Aircraft Operator"), //
     AIRCRAFT_REG_MARKING("Aircraft Reg. Marking"), //
-    AIRCRAFT_SERIAL_NUMBER("Aircraft Serial Number"), //
+    AIRCRAFT_SERIAL_NUMBER("Aircraft Serial Number", JsonType.INTEGER), //
     AUX_RADIO_LOCATING("Aux. radio-locating"), //
     BCH_2("BCH-2"), //
-    BEACON_SERIAL_NUMBER("Beacon Serial Number"), //
+    BEACON_SERIAL_NUMBER("Beacon Serial Number", JsonType.INTEGER), //
     BEACON_TYPE("Beacon Type"), //
     COARSE_POSITION("Coarse Position"), //
-    COUNTRY_CODE("Country Code"), //
+    COUNTRY_CODE("Country Code", JsonType.INTEGER), //
     COUNTRY_OF_REGISTRATION("Country of Registration"), //
-    CS_CERT_NO_PRESENT("C/S cert. no. present"), //
-    CS_TYPE_APPROVAL("C/S Type approval"), //
-    CS_TYPE_APPROVAL_NUMBER("C/S Type approval number"), //
-    DISABLED("Disabled"), //
+    CS_CERT_NO_PRESENT("C/S cert. no. present", JsonType.BOOLEAN), //
+    CS_TYPE_APPROVAL("C/S Type approval", JsonType.INTEGER), //
+    CS_TYPE_APPROVAL_NUMBER("C/S Type approval number", JsonType.INTEGER), //
+    DISABLED("Disabled", JsonType.BOOLEAN), //
     EMERGENCY_CODE("Emergency Code"), //
     EMERGENCY_CODE_FLAG("Emergency Code Flag"), //
     ENCODED_POSITION_SOURCE("Encoded Position Source"), //
     ERROR(""), //
     ERROR_CORRECTING_CODE("Error Correcting Code"), //
     FINE_POSITION("Fine Position"), //
-    FIRE_FLAG("Fire Flag"), //
+    FIRE_FLAG("Fire Flag", JsonType.BOOLEAN), //
     FIXED_BITS("Fixed bits"), //
     HEX_DATA("Hex Data"), //
     HEX_ID("Hex Id"), //
     HEX_OFFSET("Offset Position"), //
     ID_POSN("ID+POSN"), //
-    IS_LONG_MESSAGE_DEFAULT("Is Long Message Default"), //
-    LATITUDE("Latitude"), //
+    IS_LONG_MESSAGE_DEFAULT("Is Long Message Default", JsonType.BOOLEAN), //
+    LATITUDE("Latitude", JsonType.NUMBER), //
     LAT_LON("Lat Lon"), //
-    LONGITUDE("Longitude"), //
-    MEDICAL_HELP_FLAG("Medical Help Flag"), //
+    LONGITUDE("Longitude", JsonType.NUMBER), //
+    MEDICAL_HELP_FLAG("Medical Help Flag", JsonType.BOOLEAN), //
     MESSAGE_TYPE("Message Type"), //
     NATIONAL_USE("National Use"), //
     NATURE_OF_DISTRESS("Nature of Distress"), //
@@ -49,37 +50,47 @@ public enum AttributeType {
     PDF_2("PDF-2"), //
     PROTOCOL_TYPE("Protocol Type"), //
     RADIO_CALLSIGN("Radio Callsign"), //
-    RLM_CAPABILITY_TYPE_1_AUTO("RLM Capability Type-1 (Auto)"), //
-    RLM_CAPABILITY_TYPE_2_MANUAL("RLM Capability Type-2 (Manual)"), //
+    RLM_CAPABILITY_TYPE_1_AUTO("RLM Capability Type-1 (Auto)", JsonType.BOOLEAN), //
+    RLM_CAPABILITY_TYPE_2_MANUAL("RLM Capability Type-2 (Manual)", JsonType.BOOLEAN), //
     RLM_CAPABILITY_TYPE("RLM Capability Type"), //
-    RLM_RECEIVED_TYPE_1_AUTO("RLM Received Type-1 (Auto)"), //
-    RLM_RECEIVED_TYPE_2_MANUAL("RLM Received Type-2 (Manual)"), //
-    RLS_ID("RLS ID"), //
+    RLM_RECEIVED_TYPE_1_AUTO("RLM Received Type-1 (Auto)", JsonType.BOOLEAN), //
+    RLM_RECEIVED_TYPE_2_MANUAL("RLM Received Type-2 (Manual)", JsonType.BOOLEAN), //
+    RLS_ID("RLS ID", JsonType.INTEGER), //
     RLS_PROVIDER_ID("RLS Provider ID"), //
     RLS_TAC_NUMBER("RLS TAC Number"), //
-    SERIAL_NUMBER("Serial Number"), //
+    SERIAL_NUMBER("Serial Number", JsonType.INTEGER), //
     SHIP_MMSI_LAST_6_DIGITS("Ship MMSI Last 6 Digits"), //
-    SHIP_MMSI("Ship MMSI"), //
-    SHIP_SECURITY_MMSI("Ship Security MMSI"), //
+    SHIP_MMSI("Ship MMSI", JsonType.INTEGER), //
+    SHIP_SECURITY_MMSI("Ship Security MMSI", JsonType.INTEGER), //
     SPARE("Spare"), //
     SPECIFIC_BEACON_NUMBER("Specific Beacon Number"), //
-    SPECIFIC_ELT_NUMBER("Specific ELT number"), //
+    SPECIFIC_ELT_NUMBER("Specific ELT number", JsonType.INTEGER), //
     SUPPLEMENTARY_DATA("Supplementary Data"), //
     USER_PROTOCOL_TYPE("User Protocol Type"), //
-    US_MANUFACTURER_ID("US Manufacturer ID"), //
-    US_MODEL_ID("US Model ID"), //
+    US_MANUFACTURER_ID("US Manufacturer ID", JsonType.INTEGER), //
+    US_MODEL_ID("US Model ID", JsonType.INTEGER), //
     US_NATIONAL_USE("US National Use"), //
-    US_RUN_NO("US Run No."), //
-    US_SEQUENCE_NO("US Sequence No.");
+    US_RUN_NO("US Run No.", JsonType.INTEGER), //
+    US_SEQUENCE_NO("US Sequence No.", JsonType.INTEGER);
 
     private final String name;
+    private final JsonType jsonType;
 
     private AttributeType(String name) {
+        this(name, JsonType.STRING);
+    }
+
+    private AttributeType(String name, JsonType jsonType) {
         this.name = name;
+        this.jsonType = jsonType;
     }
 
     @Override
     public String toString() {
         return name;
+    }
+
+    public JsonType jsonType() {
+        return jsonType;
     }
 }
