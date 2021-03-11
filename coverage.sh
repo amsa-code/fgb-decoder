7e1e1601bffaacbcc3ad995addda5b7b0d2f88f1
David Moten <davidmoten@gmail.com>
2021-03-11 12:58:08 +1100

    add coverage.sh

--- coverage.sh
+++ coverage.sh
@@ -1,25 +1,5 @@
 #!/bin/bash
 set -e
-count=`(mvn jacoco:prepare-agent test jacoco:report >/dev/null && cat  target/site/jacoco/jacoco.xml|sed -e 's/\/>/\/>\n/g'|grep counter|tail -5|sed -e 's/.*missed="\([0-9]*\)".*/\1/g'|awk '{sum+=$1}END{print sum}')`
-counter=0
-for f in $HOME/files/*;
-do 
-  ((counter=counter+1))
-  if (( counter > 0)); then
-  echo ------------------------------------------
-  filename=${f:16}
-  dest="src/test/resources/compliance-kit/$filename"
-  if [ ! -f "$dest" ]; then
-  cp -p $f $dest 
-  echo added $dest
-  previous=$count
-  count=`(mvn jacoco:prepare-agent test jacoco:report >/dev/null && cat  target/site/jacoco/jacoco.xml|sed -e 's/\/>/\/>\n/g'|grep counter|tail -5|sed -e 's/.*missed="\([0-9]*\)".*/\1/g'|awk '{sum+=$1}END{print sum}')`
-  echo count = $count
-  if (( previous <= count )); then
-    echo removing $filename because missed coverage did not reduce 
-    count=$previous
-    rm $dest 
-  fi
-  fi
-  fi
-done
+echo Coverage totals for src/main/java classes using jacoco
+echo ----------------------------------------------------------
+mvn jacoco:prepare-agent test jacoco:report >/dev/null && cat target/site/jacoco/jacoco.xml|sed -e 's/\/>/\/>\n/g'|grep counter|tail -5
